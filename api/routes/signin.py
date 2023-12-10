86a5bd6fa3374eefb9e2bda189d8e0cee7bd0a37
zacsd123 <105044921+zacsd123@users.noreply.github.com>
2023-12-11 08:28:16 +0900

    Update signin.py

--- api/routes/signin.py
+++ api/routes/signin.py
@@ -1,5 +1,16 @@
 from flask import Blueprint, redirect, session, request, render_template
-from .Data import UserData, C
+import os
+
+C = os.path.dirname(__file__)
+db_path = os.path.join(C, "database")
+
+def UserData():
+    with sql.connect(db_path+"/userdata.db") as con:
+        con.row_factory = sql.Row
+        cur = con.cursor()
+        cur.execute("select * from userdata")
+        userdata = cur.fetchall()
+    return userdata
 
 signin = Blueprint("signin", __name__, template_folder="templates")
 
