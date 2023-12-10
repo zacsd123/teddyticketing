a5b27a0effb3647a804ee05abe320ef0ee18db28
zacsd123 <105044921+zacsd123@users.noreply.github.com>
2023-12-11 08:27:49 +0900

    Update signup.py

--- api/routes/signup.py
+++ api/routes/signup.py
@@ -4,7 +4,18 @@ import sqlite3 as sql
 import random
 import smtplib
 import re
-from .Data import UserData, db_path
+import os
+
+C = os.path.dirname(__file__)
+db_path = os.path.join(C, "database")
+
+def UserData():
+    with sql.connect(db_path+"/userdata.db") as con:
+        con.row_factory = sql.Row
+        cur = con.cursor()
+        cur.execute("select * from userdata")
+        userdata = cur.fetchall()
+    return userdata
 
 signup = Blueprint("signup", __name__, template_folder="templates")
 
